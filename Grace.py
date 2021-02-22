9e0eba97bfa27c1140b52d47239fbcae336904eb
chudyn <chudyn@yahoo.com>
2021-02-22 19:10:58 +0100

    My first commit

--- Grace.py
+++ Grace.py
@@ -1,40 +1,56 @@
-#A task was given before to just include a name and then parse it of which I did 
-#The second assignment was to parse both firstname and lastname together which was
-#done and Favour Samuel was gotten.
-#The third task isto Capitalise the first letters of "hello amazing grace"
+# Task 1 - parse a greeting and a name
+# using ctrl + f6, the command line option is set to : --name-Grace
 
-#Code for the second task.
 import argparse
 
 from argparse import ArgumentParser
 
 
-def hello(firstname, Lastname):
-    print( f'Hello {firstname} {Lastname}')
+def hello(name):
+    print( f'Hello {name}')
+
+if __name__ == '__main__':
+    args_to_parse = ArgumentParser()
+    args_to_parse.add_argument('--name', required=True, help='The name to greet')
+    args = args_to_parse.parse_args()
+
+    hello(args.name)
+
+ # When the code is run, it gives Hello Grace  
+   
+
+# Task 2 - parse both firstname and lastname
+# Using ctrl + f6, the command line option is  set to : --firstname=Favour  --lastname=Samuel
+
+def hello(firstname, lastname):
+    print( f'Hello {firstname} {lastname}')
 
 
 if __name__ == '__main__':
     args_to_parse = ArgumentParser()
     args_to_parse.add_argument('--firstname', required=True, help='The name to greet')
-    args_to_parse.add_argument('--Lastname', required=True, help='The name to greet')
+    args_to_parse.add_argument('--lastname', required=True, help='The name to greet')
     args = args_to_parse.parse_args()
 
-    hello(args.firstname,  args.Lastname)
-    
-#Code for the third task
-def hello(firstname, Lastname):
-    print( f'Hello {firstname} {Lastname}')
+    hello(args.firstname,  args.lastname)
+
+# When run, it gives Hello Favour Samuel.
+
+# Task 3 - Capitalise only the first letters of "hello amazing grace"
+# Using ctrl + f6, the command line options was set to as --firstname=amazing --lastname=grace
+# The purpose of help : "The first name to consume", "The last name to consume"
+
+def hello(firstname, lastname):
+    print( f'Hello {firstname.title()} {lastname.title()}')
 
 
 if __name__ == '__main__':
     args_to_parse = ArgumentParser()
     args_to_parse.add_argument('--firstname', required=True, help='The name to greet')
-    args_to_parse.add_argument('--Lastname', required=True, help='The name to greet')
+    args_to_parse.add_argument('--lastname', required=True, help='The name to greet')
     args = args_to_parse.parse_args()
 
-    hello(args.firstname.title(),  args.Lastname.title())
+    hello(args.firstname,  args.lastname)
 
-
- 
-    
+# When run, it will give Hello Amazing Grace
          
